962aa72f951f5c6e7c28ee5d1c059cd7b104e31f
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-05-30 11:52:52 +0530

    update app.jsx

--- portfolio/src/components/App/App.jsx
+++ portfolio/src/components/App/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
+import Home from "./../Home/Home"
+import About from "../../components/About/About"
+import Projects from "../../components/Projects/Projects"
+import Contact from "../../components/Contact/Contact"
+import Skills from "../../components/Skills/Skills"
+
 import "./App.css"
 import {
   BrowserRouter,
   Route
 } from "react-router-dom";
 
-const SampleComponent = () =>{
-  return(
-    <div>
-      <h1>This is home page</h1>
-    </div>
-  )
-}
+
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
       <div>
-        <Route path="/" component={SampleComponent}></Route>
-        <Route path="/about" component={SampleComponent}></Route>
-        <Route path="/projects" component={SampleComponent}></Route>
-        <Route path="/skills" component={SampleComponent}></Route>
-        <Route path="/contact" component={SampleComponent}></Route>
+        <Route path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/projects" component={Projects}></Route>
+        <Route path="/skills" component={Skills}></Route>
+        <Route path="/contact" component={Contact}></Route>
       </div>
       </BrowserRouter>
     </div>
