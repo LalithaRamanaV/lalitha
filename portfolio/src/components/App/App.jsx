a7f8c314fe7ec7ff24c404f0f6bc3a8b63ea986b
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-05-30 12:00:17 +0530

    updated app.jsx

--- portfolio/src/components/App/App.jsx
+++ portfolio/src/components/App/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import Home from "./../Home/Home"
-import About from "../../components/About/About"
-import Projects from "../../components/Projects/Projects"
-import Contact from "../../components/Contact/Contact"
-import Skills from "../../components/Skills/Skills"
-
+import About from "./../About/About"
+import Projects from "./../Projects/Projects"
+import Contact from "./../Contact/Contact"
+import Skills from "./../Skills/Skills"
 import "./App.css"
 import {
   BrowserRouter,
   Route
 } from "react-router-dom";
 
-
-
 const App = () => {
   return (
     <div>
       <BrowserRouter>
       <div>
-        <Route path="/" component={Home}></Route>
+        <Route path="/" component={Home} exact></Route>
         <Route path="/about" component={About}></Route>
         <Route path="/projects" component={Projects}></Route>
         <Route path="/skills" component={Skills}></Route>
