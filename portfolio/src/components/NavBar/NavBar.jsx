5717cfcd8ff47db24aadb5f6f5b8bad9b89f1abd
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-05-30 12:27:23 +0530

    build: added navbar

---
+++ portfolio/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import "./NavBar.css"
+
+class NavBar extends React.component{
+  render() {
+    return (
+        <Menu>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/projects">Projects</a>
+        </Menu>
+      );
+  }
+}
+
+export default NavBar
