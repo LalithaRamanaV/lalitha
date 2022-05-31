de8e1a40ee9708795b88c7ba969b10081c8105af
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-05-31 16:30:16 +0530

    update home.jsx

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./Home.css"
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
   return (
-    <div>Home page</div>
+    <div className="home-container">
+      <div className="header-text">
+        <h1>Hello Iam Lalitha Vadavalli</h1>
+        <p>Hi, I am a Developer. I have been working on the MERN stack since couple of months in this span I have developed multiple projects which helped me to enhance my skills. Yes, having your strong base really helps and hence I am open to adapting to whichever framework is required.</p>
+        <div className="head-bts">
+          <Link to ="/about" className="btn- btn-white">
+            <p className= "btn-text">Know more</p>
+          </Link>
+          <Link to ="/contact" className="btn btn-transparent">
+            <p className="btn-text">Connect with me</p>
+          </Link>
+          <Home/>
+        </div>
+      </div>
+    </div>
   )
 }
 
